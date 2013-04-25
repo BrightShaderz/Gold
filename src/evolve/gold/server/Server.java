352df0671819f6a9e902e5896f2ebec3688834ac
Daniel V <daniel.mogpcreations@gmail.com>
2013-04-25 17:10:37 -0400

    Edits for the day part 1a

--- src/evolve/gold/server/Server.java
+++ src/evolve/gold/server/Server.java
@@ -2,7 +2,10 @@ package evolve.gold.server;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
+import evolve.gold.server.model.entity.living.Player;
 import evolve.gold.system.logger.LogType;
 import evolve.gold.system.logger.Logger;
 import evolve.gold.system.props.ServerProperties;
@@ -12,20 +15,21 @@ import evolve.gold.system.props.ServerProperties;
 public class Server {
 	
 	private static ServerSocket connection;
+	public static List<Player> onlineUsers = new ArrayList<Player>();
 	private static Logger logger;
 	private final int minecraftVersion = 60;
 	private static Boolean alive = false;
     
     public void start() {
+
     	this.alive = true;
-    	
+
     	while(alive) {
     		try{
     			connection = new ServerSocket(ServerProperties.getPort());
     			new Thread(){
-					public void run()
-					{
-						Connect();
+					public void run() {
+						connect();
 					}
 				}.start();
     		} catch(Exception e) {
@@ -42,13 +46,13 @@ public class Server {
 		
 	}
 	
-	public static void Connect()
+	public static void connect()
 	{
 		while (true)
 		{
 			try{
 				Socket client = connection.accept();
-				//TODO add new player & send packets.
+				Player p = new Player(client);
 			} catch (Exception e){
 				logger.Log(e.toString(), LogType.ERROR);
 			}
--- src/evolve/gold/server/model/entity/living/Player.java
+++ src/evolve/gold/server/model/entity/living/Player.java
@@ -1,13 +1,25 @@
 package evolve.gold.server.model.entity.living;
 
+import java.net.Socket;
+
 import evolve.gold.server.model.entity.LivingEntity;
+import evolve.gold.system.logger.LogType;
+import evolve.gold.system.logger.Logger;
 
 public class Player extends LivingEntity {
 
+	private Socket s;
+	private Logger logger;
 	private String loginName;
 	private String displayName;
+	private boolean isConnected = false;
 	
-	public Player() {
+	public Player(Socket socket) {
+		try {
+			this.s = socket;
+		} catch (Exception e) {
+			logger.Log(e.toString(), LogType.ERROR);
+		}
 	}
 
 }
